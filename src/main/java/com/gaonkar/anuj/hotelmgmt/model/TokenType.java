20de6ae661dace2fc2c4919bbe1865cd4043f249
Anuj Gaonkar <anujgaonkar11@gmail.com>
2023-12-29 19:22:10 +0530

    init commit

---
+++ src/main/java/com/gaonkar/anuj/hotelmgmt/model/TokenType.java
@@ -0,0 +1,5 @@
+package com.gaonkar.anuj.hotelmgmt.model;
+
+public enum TokenType {
+  BEARER
+}
---
+++ src/test/java/com/gaonkar/anuj/hotelmgmt/HotelMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaonkar.anuj.hotelmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
